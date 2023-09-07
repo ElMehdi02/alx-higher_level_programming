3b668d7682110f328caad17740ee13baaaa81aca
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-09-07 14:26:24 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,20 +3,20 @@
 if __name__ == "__main__":
     """Build my own calculator!"""
     import sys
-    from calculator_1 import mul, div, add, sub
+    from calculator_1 import mul, sub, add, div
 
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        op = sys.argv[2]
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = sys.argv[2]
 
-        dic = {"+": add, "-": sub, "*": mul, "/": div}
+    dic = {"+": add, "-": sub, "*": mul, "/": div}
 
-        if op not in dic:
-            print("Unknown operator, Available operators: +, -, * and /")
-            sys.exit(1)
+    if op not in dic:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-            print("{} {} {} = {}".format(a, op, b, dic[op](a,b)))
+    print("{} {} {} = {}".format(a, op, b, dic[op](a, b)))
