264246a4e891da9c10a386f32c9976e4e720e48f
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-09-07 14:58:21 +0100

    Update 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    """Diassembly python bytecode."""
     from magic_calculation_102 import add, sub
 
     if a < b:
-        c = add(a, b)
+        c = a
         for i in range(4, 6):
             c = add(c, i)
-        return (c)
-
+        return add(c, b)
     else:
-        return(sub(a, b))
+        return sub(a, b)
