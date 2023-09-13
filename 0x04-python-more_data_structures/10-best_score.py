07c9e257145d87d2460d28ca5cb1a6523b97dfd3
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-09-13 12:16:50 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,8 +5,8 @@ def best_score(a_dictionary):
     best = -1
     if (a_dictionary is None):
         return key
-    for k, v in a_dictionay.items():
+    for k, v in a_dictionary.items():
         if v > best:
             best = v
-            key = v
-            return key
+            key = k
+    return key
