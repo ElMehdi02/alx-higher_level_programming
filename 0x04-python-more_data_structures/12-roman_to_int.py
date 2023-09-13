224841d3b03113eac908d2029998696a1b4207dc
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-09-13 12:18:39 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
 def roman_to_int(roman_string):
-    if not isinstance(roamn_string, str):
+    if not isinstance(roman_string, str):
         return 0
-    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D':500, 'M': 1000}
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
-    prev_alue = 0
+    prev_value = 0
     for char in roman_string:
         if char in dic:
             current_value = dic[char]
@@ -13,7 +12,7 @@ def roman_to_int(roman_string):
                 total += current_value - 2 * prev_value
             else:
                 total += current_value
-                prev_value = current_value
-            else:
-                return 0
-            return total
+            prev_value = current_value
+        else:
+            return 0
+    return total
