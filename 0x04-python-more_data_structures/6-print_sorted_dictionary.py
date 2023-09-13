5b1bccab5f09ae97c651ba5f678398c6f9d38f2c
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-09-13 12:11:56 +0100

    Update 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    sorted-dict = dict(sorted(a_dictionary.items()))
+    sorted_dict = dict(sorted(a_dictionary.items()))
     for k, v in sorted_dict.items():
         print('{}: {}'.format(k, v))
