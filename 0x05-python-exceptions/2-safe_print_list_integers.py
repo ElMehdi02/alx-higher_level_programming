b5023d74eb6987867389a4886c3a695eeeeff2af
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-09-25 11:40:57 +0100

    Update 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             iter += 1
         except (TypeError, ValueError):
             continue
-        print()
-        return iter
+    print()
+    return iter
