4263cdf965922b8a47c4b593bcbe108e422b5c97
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-09-25 11:39:10 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    ""print x element of a list.
+    """print x elements of a list.
 
     Args:
         my_list (list): list of all elements.
         x (int): the number of time you need to iterate over the list
-        Return:
-            the number of elements printed.
-            """
-            iter = 0
-            for i in range(0, x):
-            try:
+    Return:
+        The number of elements printed.
+    """
+    iter = 0
+    for i in range(0, x):
+        try:
             print("{}".format(my_list[i]), end="")
             iter += 1
-            except IndexError:
+        except IndexError:
             break
-            print()
-            return iter
+    print()
+    return iter
