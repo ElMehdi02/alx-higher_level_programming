fbd88133c0dabac62db811c73ee9f99fd16dd690
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-10-02 12:05:00 +0100

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,56 +1,46 @@
-#!/usr/bin/python3
 import sys
+"""Solves the N-queens puzzle"""
 
-
-def is_safe(board, row, col, N):
-    for i in range(row):
-        if board[i][col] == 1:
-            return False
-        if col - (row - i) >= 0 and board[i][col - (row - i)] == 1:
-            return False
-        if col + (row - i) < N and board[i][col + (row - i)] == 1:
-            return False
-    return True
-
-
-def solve_nqueens(board, row, N, solutions):
-    if row == N:
-        solutions.append([[r, c] for r, c in enumerate(board)])
-        return
-
-    for col in range(N):
-        if is_safe(board, row, col, N):
-            board[row][col] = 1
-            solve_nqueens(board, row + 1, N, solutions)
-            board[row][col] = 0
-
-
-def print_solutions(solutions):
-    for solution in solutions:
-        print(solution)
-
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
+def nqueens(N):
+    """ Check if N is an integer"""
     try:
-        N = int(sys.argv[1])
+        N = int(N)
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-
+        
+    """ Check if N is at least 4 """
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
+        
+    """ Initialize the board """
+    board = [-1] * N
 
-    board = [[0 for _ in range(N)] for _ in range(N)]
-    solutions = []
+    """Recursive function to find all solutions """
+    def place_queen(row, column):
+        """ Check if the queen can be placed in this column """
+        for i in range(row):
+            if board[i] == column or \
+               board[i] - i == column - row or \
+               board[i] + i == column + row:
+                return False
+        return True
+    
+    def solve(row):
+        if row == N:
+            print(" ".join(str(i+1) for i in board))
+            return
+        for col in range(N):
+            if place_queen(row, col):
+                board[row] = col
+                solve(row + 1)
+                
+    solve(0)
 
-    solve_nqueens(board, 0, N, solutions)
-    print_solutions(solutions)
+""" Check if the correct number of arguments is provided """
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
 
-
-if __name__ == "__main__":
-    main()
+nqueens(sys.argv[1])
