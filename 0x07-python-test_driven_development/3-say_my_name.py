85b500397b7efd410f526f8a8477ca9b63291250
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-10-03 19:22:22 +0100

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,10 +6,10 @@ def say_my_name(first_name, last_name=""):
     """Print a name.
 
     Args:
-         first_name (str): The fisrt name to print.
-         last_name (str): The last name to print.
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
     Raises:
-         TypeError: If either of first_name or last_name are not strings.
+        TypeError: If either of first_name or last_name are not strings.
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
