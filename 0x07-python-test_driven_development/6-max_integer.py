db935df40e5308145ee3b0cd9206a5d100e8eafa
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-10-03 19:23:45 +0100

    Update 6-max_integer.py

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -4,8 +4,8 @@
 
 
 def max_integer(list=[]):
-    """Functions to find and return the max integer in a listof integers
-          If the list is empty, the function returns None
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
     """
     if len(list) == 0:
         return None
@@ -14,5 +14,5 @@ def max_integer(list=[]):
     while i < len(list):
         if list[i] > result:
             result = list[i]
-            i += 1
-            return result
+        i += 1
+    return result
