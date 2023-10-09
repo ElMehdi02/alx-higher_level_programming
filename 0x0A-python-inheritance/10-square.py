da3fb67568d0663b1d2363f82a15dd70aa0abc99
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-10-09 23:01:56 +0100

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,11 +4,21 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square.
-
-    Args:
-    size (int): The size of the new square.
     """
-    self.integer_validator("size", size)
-    super().__init__(size, size)
-    self.__size = size
+    This is the documentation for the Square class.
+    """
+
+    def __init__(self, size):
+        """
+        Initialize the Square instance with size.
+        """
+        self.__size = 0
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """
+        Return the string representation of the Square instance.
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
