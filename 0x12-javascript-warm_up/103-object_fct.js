11fb987e3517ff0598e1566940c0f3f179cbe1be
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-12-11 13:56:54 +0100

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const myObject = {
-    type: 'object',
-    value: 12
+  type: 'object',
+  value: 12
 };
 console.log(myObject);
 myObject.incr = function () {
-    this.value++;
+  this.value++;
 };
 myObject.incr();
 console.log(myObject);
