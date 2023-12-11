57a48e709d3304fd1d89a96ef196ccb55c209193
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-12-11 13:41:56 +0100

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined) {
-    console.log('NO argumen');
+  console.log('No argument');
 } else {
-    console.log(process.argv[2]);
+  console.log(process.argv[2]);
 }
