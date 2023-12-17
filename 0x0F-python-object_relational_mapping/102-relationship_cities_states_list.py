31edb1af66a81e5fe043099ebf2fc8380b7f099e
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-12-17 22:38:53 +0100

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,36 +1,21 @@
 #!/usr/bin/python3
+""" prints the State object with the name passed as argument from the database
 """
-Script to list City objects from the database using SQLAlchemy
-"""
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from relationship_city import Base, City
-from relationship_state import State
 import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>"
-              .format(sys.argv[0]))
-        sys.exit(1)
-
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Connect to the MySQL server running on localhost at port 3306
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        mysql_username, mysql_password, database_name))
-
-    # Create a session
-    session = Session(engine)
-
-    # Retrieve all City objects and their associated State objects
-    cities = session.query(City).order_by(City.id).all()
-
-    for city in cities:
-        state_name = city.state.name if city.state else "N/A"
-        print("{}: {} -> {}".format(city.id, city.name, state_name))
-
-    # Close the session
-    session.close()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        for city_ins in instance.cities:
+            print(city_ins.id, city_ins.name, sep=": ", end="")
+            print(" -> " + instance.name)
