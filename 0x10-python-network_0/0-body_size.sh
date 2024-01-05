589c21cbb37f2914d3afd71b5ec32bc8b6660b47
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2024-01-05 19:31:25 +0100

    0x10-python-network_0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL, sends a request and displays the size of the body of the response
-curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
