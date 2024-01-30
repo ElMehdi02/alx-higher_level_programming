e3022cce28459036676116b55b25411fa9c98480
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2024-01-30 12:51:55 +0100

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-	if (error) console.log(error);
+
+const filePath = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(filePath, data, 'utf8', function(err) {
+    if (err) {
+        console.log(err);
+    }
 });
